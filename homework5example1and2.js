a40c52dd114a64e0a28b70c0386cc1acc07bd26d
RomanBurningMan <drabadil1@mail.ru>
2016-07-17 22:26:16 +0300

    There are two exmple in one

---
+++ homework5example1and2.js
@@ -0,0 +1,27 @@
+module.export = {prepend: prepend,
+				deleteTextNodes: deleteTextNodes};
+
+// ДЗ - 1
+
+function prepend (container, newElem){
+	var firstCh = container.firstElementChild;
+		container.insertBefore(newElem, firstCh);
+	return 'Функция отработала';	
+}
+
+// ДЗ - 2
+
+function deleteTextNodes(parentElem){
+	var i,
+		elemArr = parentElem.childNodes;
+	for (i=0; i<elemArr.length; i++){
+		itemArr = parentElem.childNodes[i];
+		if (itemArr.nodeType === 3){
+			parentElem.removeChild(elemArr[i]);
+		}
+	}
+	return elemArr;
+}
+
+
+
